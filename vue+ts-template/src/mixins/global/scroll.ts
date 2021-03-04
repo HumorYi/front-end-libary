32305755c9adac7aa5e918fc087cdf9f63ad9a2c
HumorYi <bamboo8493@126.com>
2021-03-04 12:09:06 +0800

    perfect: scroll util

--- vue+ts-template/src/mixins/global/scroll.ts
+++ vue+ts-template/src/mixins/global/scroll.ts
@@ -1,24 +1,45 @@
 import { Vue, Component } from 'vue-property-decorator'
+
+const elements = [window, document.documentElement, document.body]
+
 @Component
 export default class Scroll extends Vue {
   isFixedByScroll = false
-  // 可根据项目修改滚动栏距离顶部值
-  scrollFixedTopDistance = 0
+  scrollFixedTopDistance = 70
 
   created() {
-    window.addEventListener('scroll', this.scroll)
+    this.addEventListener()
+
+    window.addEventListener('resize', this.scroll, false)
   }
 
   destroyed() {
-    window.removeEventListener('scroll', this.scroll)
+    this.removeEventListener()
+
+    window.removeEventListener('resize', this.scroll, false)
   }
 
-  scroll() {
+  addEventListener() {
+    elements.forEach(
+      item => item.addEventListener('scroll', this.scroll),
+      false
+    )
+  }
+
+  removeEventListener() {
+    elements.forEach(
+      item => item.removeEventListener('scroll', this.scroll),
+      false
+    )
+  }
+
+  scroll(evt: Event) {
     // 滚动栏滚动一次间距
     const scrollSpace = 100
 
     this.isFixedByScroll =
       (this['hasScrollHeight'] ? this['hasScrollHeight']() : true) &&
-      window.scrollY > this.scrollFixedTopDistance + scrollSpace
+      (document.documentElement.scrollTop || document.body.scrollTop) >
+        this.scrollFixedTopDistance + scrollSpace
   }
 }
